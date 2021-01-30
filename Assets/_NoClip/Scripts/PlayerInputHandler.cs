9783ce253b302af170472b7f19785d38fe88adb4
Jake Troughton <jaketroughton94@gmail.com>
2021-01-30 15:50:07 +1100

    Add noclip movement

--- Assets/_NoClip/Scripts/PlayerInputHandler.cs
+++ Assets/_NoClip/Scripts/PlayerInputHandler.cs
@@ -39,7 +39,6 @@ public class PlayerInputHandler : MonoBehaviour
         if (CanProcessInput())
         {
             Vector3 move = new Vector3(Input.GetAxisRaw(GameConstants.k_AxisNameHorizontal), 0f, Input.GetAxisRaw(GameConstants.k_AxisNameVertical));
-
             // constrain move input to a maximum magnitude of 1, otherwise diagonal movement might exceed the max move speed defined
             move = Vector3.ClampMagnitude(move, 1);
 
