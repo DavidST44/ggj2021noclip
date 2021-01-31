9ee7b1601b0951eea8a2f18dddc288c9865abdc8
Nicholas Maurer <nicholasallenmaurer@gmail.com>
2021-01-31 14:01:15 +1100

    Created a HUD prefab, added HealthBar to it and changed HealthBar.cs to look for the player controller

--- Assets/_NoClip/Scripts/HealthBar.cs
+++ Assets/_NoClip/Scripts/HealthBar.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,6 +8,11 @@ public class HealthBar : MonoBehaviour
 {
     public PlayerHealth playerHealth;
     public RectTransform healthBar;
+
+    private void Start()
+    {
+        playerHealth = FindObjectOfType<PlayerHealth>();
+    }
 
     private void Update()
     {
