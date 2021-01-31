66b55a2762ceb13a24b0b57589e089b29c62e22d
Jake Troughton <jaketroughton94@gmail.com>
2021-01-31 14:31:50 +1100

    Add items
    Items are collected to unlock surrounding shells

---
+++ Assets/_NoClip/Scripts/Item.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            ++PlayerHealth.itemsCollected;
+            Destroy(gameObject);
+        }
+
+    }
+}
