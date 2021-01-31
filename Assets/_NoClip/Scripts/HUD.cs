f36bcf928ba6963112ab08bee06c6e80cff0d87e
Nicholas Maurer <nicholasallenmaurer@gmail.com>
2021-01-31 15:56:02 +1100

    Added death splash to hud prefab

--- Assets/_NoClip/Scripts/HUD.cs
+++ Assets/_NoClip/Scripts/HUD.cs
@@ -1,18 +1,48 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class HUD : MonoBehaviour
 {
+    public CanvasGroup m_DeathSpalshCanvasGroup;
+
+    private PlayerControllerClip m_playerControllerClip;
     // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-        
+        HideDeathSplash();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (!m_playerControllerClip)
+        {
+            return;
+        }
+
+        if (m_playerControllerClip.IsAlive())
+        {
+            HideDeathSplash();
+        }
+        else
+        {
+            ShowDeathSplash();
+        }
+    }
+
+    void ShowDeathSplash()
+    {
+        m_DeathSpalshCanvasGroup.alpha = 1;
+    }
+
+    void HideDeathSplash()
+    {
+        m_DeathSpalshCanvasGroup.alpha = 0;
+    }
+
+    public void OnRespawnButtonPressed()
+    {
+        m_playerControllerClip.Respawn();
     }
 }
